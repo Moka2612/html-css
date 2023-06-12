780eb93db85a0d46e3a53cc8af7d0d47b1bac02e
Moka2612 <tranhuy261292@gmail.com>
2023-06-12 15:16:38 +0700

    Hoàn thành project Javascript

---
+++ JavaScript/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '129d3623-9aa0-4e3a-b1ef-546713ba6660';
+export const MODAL_CLOSE_SEC = 2.5;
